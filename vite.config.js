d34d8710ada0b35a66718fdeefe56fe651f0a727
Pritesh <bhuravanepritesh@gmail.com>
2025-06-25 19:33:20 +0530

    render commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: parseInt(process.env.PORT || "") || 5173,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: parseInt(process.env.PORT || "") || 5173,
+    allowedHosts: ["newsportal-4r2d.onrender.com"], // ✅ Fixed
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
